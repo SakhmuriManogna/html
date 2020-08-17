fe8b4be6e0efd0ccf933c906d74a601437f12898
SakhmuriManogna <68653693+SakhmuriManogna@users.noreply.github.com>
2020-08-17 22:35:43 +0530

    Add files via upload

---
+++ AccessSpecifiers.java
@@ -0,0 +1,13 @@
+package mano;
+
+public class AccessSpecifiers {
+
+	public static void main(String[] args) {
+		/* public-- can access all classes in package outside package
+		 protected-- will access full package and other child classes outside packages
+		 default--within the package
+		 private-- only inside the  class */
+
+	}
+
+}
